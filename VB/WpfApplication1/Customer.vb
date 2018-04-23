2cec42eea17f15c9a3668208ee2282a16761a448
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ VB/WpfApplication1/Customer.vb
@@ -0,0 +1,53 @@
+﻿Imports Microsoft.VisualBasic
+Imports System
+Imports System.Collections.Generic
+Imports System.Linq
+Imports System.Text
+Imports DevExpress.Xpo
+Imports System.Windows
+Imports System.ComponentModel
+Imports System.Windows.Controls
+
+Namespace WpfApplication1
+	Public Class ContactItem
+		Private privateName As String
+		Public Property Name() As String
+			Get
+				Return privateName
+			End Get
+			Set(ByVal value As String)
+				privateName = value
+			End Set
+		End Property
+		Private privateSalary As Integer
+		Public Property Salary() As Integer
+			Get
+				Return privateSalary
+			End Get
+			Set(ByVal value As Integer)
+				privateSalary = value
+			End Set
+		End Property
+		Private privateSalaryLimitation As Integer
+		Public Property SalaryLimitation() As Integer
+			Get
+				Return privateSalaryLimitation
+			End Get
+			Set(ByVal value As Integer)
+				privateSalaryLimitation = value
+			End Set
+		End Property
+
+		Private privateIsFired As Boolean
+		Public Property IsFired() As Boolean
+			Get
+				Return privateIsFired
+			End Get
+			Set(ByVal value As Boolean)
+				privateIsFired = value
+			End Set
+		End Property
+	End Class
+
+
+End Namespace
---
+++ VB/WpfApplication1/Window1.xaml.vb
@@ -0,0 +1,107 @@
+﻿Imports Microsoft.VisualBasic
+Imports System
+Imports System.Collections.Generic
+Imports System.Linq
+Imports System.Text
+Imports System.Windows
+Imports System.Windows.Controls
+Imports System.Windows.Data
+Imports System.Windows.Documents
+Imports System.Windows.Input
+Imports System.Windows.Media
+Imports System.Windows.Media.Imaging
+Imports System.Windows.Navigation
+Imports System.Windows.Shapes
+Imports DevExpress.Xpo
+Imports System.ComponentModel
+Imports DevExpress.Xpf.Grid
+Imports System.Windows.Threading
+Imports DevExpress.Xpf.Editors.Settings
+Imports System.Threading
+Imports DevExpress.Data
+Imports System.Windows.Markup
+
+Namespace WpfApplication1
+	''' <summary>
+	''' Interaction logic for Window1.xaml
+	''' </summary>
+	Partial Public Class Window1
+		Inherits Window
+		Private coll As BindingList(Of ContactItem)
+		Public Sub New()
+			InitializeComponent()
+			coll = New BindingList(Of ContactItem)()
+			Dim f1 As New ContactItem()
+			f1.Name = "One Two Three Four Five Six Seven Eight Nine Ten Eleven Twelve"
+			f1.Salary = 13
+			f1.SalaryLimitation = 30
+			f1.IsFired=False
+			coll.Add(f1)
+			f1 = New ContactItem()
+			f1.Name = "One"
+			f1.Salary = 23
+			f1.SalaryLimitation = 50
+
+			coll.Add(f1)
+			f1 = New ContactItem()
+			f1.Name = "One"
+			f1.Salary = 23
+			f1.SalaryLimitation = 50
+
+			coll.Add(f1)
+			f1 = New ContactItem()
+			f1.Name = "Two"
+			f1.Salary = 23
+			f1.SalaryLimitation = 50
+
+			coll.Add(f1)
+			f1 = New ContactItem()
+			f1.Name = "Two"
+			f1.Salary = 23
+			f1.SalaryLimitation = 50
+
+			coll.Add(f1)
+			f1 = New ContactItem()
+			f1.Name = "Two"
+			f1.Salary = 23
+			f1.SalaryLimitation = 50
+
+			coll.Add(f1)
+
+			f1 = New ContactItem()
+			f1.Name = "One"
+			f1.Salary = 23
+			f1.SalaryLimitation = 50
+
+			coll.Add(f1)
+			f1 = New ContactItem()
+			f1.Name = "Two"
+			f1.Salary = 23
+			f1.SalaryLimitation = 50
+
+			coll.Add(f1)
+			f1 = New ContactItem()
+			f1.Name = "One"
+			f1.Salary = 23
+			f1.SalaryLimitation = 50
+
+			coll.Add(f1)
+
+			f1 = New ContactItem()
+			f1.Name = "Two"
+			f1.Salary = 23
+			f1.SalaryLimitation = 50
+
+			coll.Add(f1)
+
+
+			grid.DataSource = coll
+
+
+		End Sub
+
+
+
+	End Class
+
+End Namespace
