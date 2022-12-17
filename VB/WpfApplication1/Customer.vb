c8e3618d77f718c16c5f1ced000e32367bb86b91
DevExpressExampleBot <fakeEmail>
2022-12-17 15:09:02 +0400

    Source auto update [skip ci]

--- VB/WpfApplication1/Customer.vb
+++ VB/WpfApplication1/Customer.vb
@@ -1,53 +1,18 @@
-﻿Imports Microsoft.VisualBasic
-Imports System
-Imports System.Collections.Generic
-Imports System.Linq
-Imports System.Text
-Imports DevExpress.Xpo
-Imports System.Windows
 Imports System.ComponentModel
 Imports System.Windows.Controls
 
 Namespace WpfApplication1
-	Public Class ContactItem
-		Private privateName As String
-		Public Property Name() As String
-			Get
-				Return privateName
-			End Get
-			Set(ByVal value As String)
-				privateName = value
-			End Set
-		End Property
-		Private privateSalary As Integer
-		Public Property Salary() As Integer
-			Get
-				Return privateSalary
-			End Get
-			Set(ByVal value As Integer)
-				privateSalary = value
-			End Set
-		End Property
-		Private privateSalaryLimitation As Integer
-		Public Property SalaryLimitation() As Integer
-			Get
-				Return privateSalaryLimitation
-			End Get
-			Set(ByVal value As Integer)
-				privateSalaryLimitation = value
-			End Set
-		End Property
 
-		Private privateIsFired As Boolean
-		Public Property IsFired() As Boolean
-			Get
-				Return privateIsFired
-			End Get
-			Set(ByVal value As Boolean)
-				privateIsFired = value
-			End Set
-		End Property
-	End Class
+    Public Class ContactItem
 
+        Public Property Name As String
 
+        Public Property Salary As Integer
+
+        Public Property SalaryLimitation As Integer
+
+        Public Property [Date] As Date
+
+        Public Property IsFired As Boolean
+    End Class
 End Namespace
