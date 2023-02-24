d79fe955147857cd9d4fc331688f20bdb510145f
AndreySlabov <andrey.slabov@devexpress.com>
2023-02-24 15:36:25 +0400

    Update Sample

---
+++ CS/WpfApplication1/Product.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace WpfApplication1 {
+    public class Product {
+        public string ProductName { get; set; }
+        public string Country { get; set; }
+        public string City { get; set; }
+        public int Quantity { get; set; }
+        public DateTime OrderDate { get; set; }
+    }
+}
--- CS/WpfApplication1/Window1.xaml.cs
+++ CS/WpfApplication1/Window1.xaml.cs
@@ -1,105 +1,57 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿using DevExpress.Mvvm;
+using System;
+using System.Collections.ObjectModel;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
-using DevExpress.Xpo;
-using System.ComponentModel;
-using DevExpress.Xpf.Grid;
-using System.Windows.Threading;
-using DevExpress.Xpf.Editors.Settings;
-using System.Threading;
-using DevExpress.Data;
-using System.Windows.Markup;
 
 namespace WpfApplication1 {
-    /// <summary>
-    /// Interaction logic for Window1.xaml
-    /// </summary>
     public partial class Window1 : Window {
-        BindingList<ContactItem> coll;
         public Window1() {
             InitializeComponent();
-            coll = new BindingList<ContactItem>();
-            ContactItem f1 = new ContactItem();
-            f1.Name = "One Two Three Four Five Six Seven Eight Nine Ten Eleven Twelve";
-            f1.Salary = 13;
-            f1.SalaryLimitation = 30;
-            f1.IsFired=false;
-            coll.Add(f1);
-            f1 = new ContactItem();
-            f1.Name = "One";
-            f1.Salary = 23;
-            f1.SalaryLimitation = 50;
-            
-            coll.Add(f1);
-            f1 = new ContactItem();
-            f1.Name = "One";
-            f1.Salary = 23;
-            f1.SalaryLimitation = 50;
-
-            coll.Add(f1);
-            f1 = new ContactItem();
-            f1.Name = "Two";
-            f1.Salary = 23;
-            f1.SalaryLimitation = 50;
-
-            coll.Add(f1);
-            f1 = new ContactItem();
-            f1.Name = "Two";
-            f1.Salary = 23;
-            f1.SalaryLimitation = 50;
-
-            coll.Add(f1);
-            f1 = new ContactItem();
-            f1.Name = "Two";
-            f1.Salary = 23;
-            f1.SalaryLimitation = 50;
-
-            coll.Add(f1);
-
-            f1 = new ContactItem();
-            f1.Name = "One";
-            f1.Salary = 23;
-            f1.SalaryLimitation = 50;
-
-            coll.Add(f1);
-            f1 = new ContactItem();
-            f1.Name = "Two";
-            f1.Salary = 23;
-            f1.SalaryLimitation = 50;
-
-            coll.Add(f1);
-            f1 = new ContactItem();
-            f1.Name = "One";
-            f1.Salary = 23;
-            f1.SalaryLimitation = 50;
-
-            coll.Add(f1);
-
-            f1 = new ContactItem();
-            f1.Name = "Two";
-            f1.Salary = 23;
-            f1.SalaryLimitation = 50;
-
-            coll.Add(f1);
-
-     
-            grid.ItemsSource = coll;
-
-
         }
-
-        
-
     }
-
+    public class ViewModel : ViewModelBase {
+        public ViewModel() {
+            Source = ProductsDataModel.GetProducts();
+        }
+        public ObservableCollection<Product> Source { get; }
+    }
+    public class ProductsDataModel {
+        public static ObservableCollection<Product> GetProducts() {
+            ObservableCollection<Product> products = new ObservableCollection<Product> {
+                new Product() {
+                    ProductName = "Chang", Country = "UK", City = "Cowes",
+                    Quantity = 10, OrderDate = new DateTime(2021, 10, 23)
+                },
+                new Product() {
+                    ProductName = "Gnocchi di nonna Alice", Country = "Italy", City = "Reggio Emilia",
+                    Quantity = 16, OrderDate = new DateTime(2021, 10, 22)
+                },
+                new Product() {
+                    ProductName = "Ravioli Angelo", Country = "Brazil", City = "Rio de Janeiro", 
+                    Quantity = 12, OrderDate = new DateTime(2021, 10, 21)
+                },
+                new Product() {
+                    ProductName = "Tarte au sucre", Country = "Germany", City = "Leipzig",
+                    Quantity = 50, OrderDate = new DateTime(2021, 10, 15)
+                },
+                new Product() {
+                    ProductName = "Steeleye Stout", Country = "USA", City = "Denver",
+                    Quantity = 20, OrderDate = new DateTime(2021, 10, 8)
+                },
+                new Product() {
+                    ProductName = "Pavlova", Country = "Austria", City = "Graz",
+                    Quantity = 52, OrderDate = new DateTime(2021, 10, 1)
+                },
+                new Product() {
+                    ProductName = "Longlife Tofu", Country = "USA", City = "Boise",
+                    Quantity = 120, OrderDate = new DateTime(2021, 9, 17)
+                },
+                new Product() {
+                    ProductName = "Alice Mutton", Country = "Mexico", City = "México D.F.",
+                    Quantity = 15, OrderDate = new DateTime(2021, 9, 25)
+                }
+            };
+            return products;
+        }
+    }
 }
